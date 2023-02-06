210e9084d6b1c0947836e3276394ed34a8c07fc3
Rogerio Yudi Horauti <rogeriohorauti@gmail.com>
2023-02-06 12:57:42 -0300

    Add files via upload

---
+++ OAuth/secret.js
@@ -0,0 +1,19 @@
+/**
+ * secret.js
+ * @NApiVersion 2.1
+ * @NModuleScope Public
+ */
+
+define([], function() {
+    return {
+        consumer: {
+            public: 'ecc12151de3eec0e552f91ba5661096ac26ca419faf8b22a26567257a327d3c8',
+            secret: 'ecd21bb3770cd878e0bdcac4a94535058ea801e9d486630a0b6059bb8d0b9340'
+        },
+        token: {
+            public: '103c2737f4b97cda66cc7bac0bf28b2acc695e9c19846b4fe27b0390ee35c058',
+            secret: '3b7c5acb94763bc6a6c2b2485aa5b827e96c08a009a2de59d31cbfccad0f33f1'
+        },
+        realm: '7252561_SB1'
+    }
+});
